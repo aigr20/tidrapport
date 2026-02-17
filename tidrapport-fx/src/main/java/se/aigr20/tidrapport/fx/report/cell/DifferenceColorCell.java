09de42e31af627279374cb3a1d801aa61c755817
Wilhelm Gren <wilhelmgren@outlook.com>
2026-02-17 22:02:00 +0100

    Få inte NPE på rader utan skillnad
    Verkar bara hända på Windows

--- tidrapport-fx/src/main/java/se/aigr20/tidrapport/fx/report/cell/DifferenceColorCell.java
+++ tidrapport-fx/src/main/java/se/aigr20/tidrapport/fx/report/cell/DifferenceColorCell.java
@@ -2,6 +2,8 @@ package se.aigr20.tidrapport.fx.report.cell;
 
 import se.aigr20.tidrapport.fx.report.model.ReportRow;
 
+import java.time.Duration;
+
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableCell;
 
@@ -9,16 +11,22 @@ public class DifferenceColorCell extends TreeTableCell<ReportRow, String> {
   @Override
   protected void updateItem(final String item, final boolean empty) {
     super.updateItem(item, empty);
-    if (empty || item == null) {
+    final TreeItem<ReportRow> rowItem = getTableRow().getTreeItem();
+    if (empty ||
+        item == null ||
+        rowItem == null ||
+        rowItem.getValue() == null ||
+        rowItem.getValue().getDifference() == null) {
       setText("");
       setStyle("");
       return;
     }
     setText(item);
-    final TreeItem<ReportRow> rowItem = getTableRow().getTreeItem();
-    if (rowItem != null && rowItem.getValue().getDifference().isPositive()) {
+
+    final Duration difference = rowItem.getValue().getDifference();
+    if (difference.isPositive()) {
       setStyle("-fx-text-fill: red");
-    } else if (rowItem != null) {
+    } else {
       setStyle("-fx-text-fill: green");
     }
   }
